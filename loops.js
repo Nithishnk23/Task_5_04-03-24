0d26a4ebe096538e86a44c95bb9aa73e31aafd62
Nithishnk23 <knithish389@gmail.com>
2024-03-05 21:28:28 +0530

    Task_5_04-03-24

---
+++ loops.js
@@ -0,0 +1,40 @@
+let json = [{
+    "id" : "Nithish", 
+    "msg"   : "For the given JSON iterate over all for loops (for, for in, for of, forEach)",
+    "task" : "zen portal task",
+    "mail": "knithish389@gmail.com"
+},
+{
+    "id" : "NK", 
+    "msg"   : "For the given JSON iterate over all for loops (for, for in, for of, forEach)",
+    "task" : "zen portal task",
+    "mail": "knithish389@gmail.com"
+}];
+
+//for loop
+for(var i = 0; i < json.length; i++) {
+    var obj = json[i];
+
+    console.log(obj.id);
+    console.log(obj.msg);
+    console.log(obj.task);
+    console.log(obj.mail);
+
+}
+
+//for Each
+json.forEach(function(obj) { console.log(obj.msg); });
+
+//for In
+for (var key in json) {
+if (json.hasOwnProperty(key)) {
+  console.log(json[key].id);
+}
+}
+
+//for Of
+let text = "";
+for (let x of json[key].id) {
+ text += x; 
+}
+ console.log(text);
